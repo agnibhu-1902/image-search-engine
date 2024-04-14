9bc349597305cb88e4e70b6410152cf2fcb7fd69
Agnibhu Mandal <agnibhu.mandal@gmail.com>
2024-04-15 01:54:18 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const access_key = '75LR80E9ayKIHljlXMIJnq3fQ_nSw3e0OfLYsU6aayo'
+const searchForm = document.getElementById('search-form')
+const searchBox = document.getElementById('search-box')
+const searchResult = document.getElementById('search-result')
+const showMoreButton = document.getElementById('show-more-btn')
+
+let keyword = ''
+let page = 1
+const perPage = 12
+
+const searchImages = async () => {
+    keyword = searchBox.value
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&per_page=${perPage}&client_id=${access_key}`
+    if (page === 1)
+        searchResult.innerHTML = ""
+    const response = await fetch(url)
+    const data = await response.json()
+    const results = data.results
+    results.map((result) => {
+        const image = document.createElement('img')
+        image.src = result.urls.regular
+        const imageLink = document.createElement('a')
+        imageLink.href = result.links.html
+        imageLink.target = '_blank'
+        imageLink.appendChild(image)
+        searchResult.appendChild(imageLink)
+    })
+    showMoreButton.style.display = 'block'
+}
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    page = 1
+    searchImages()
+})
+
+showMoreButton.addEventListener('click', () => {
+    page++
+    searchImages()
+})
